7a0cf3cb87cadfd02be4e0483fd7bad211e35382
Abhik Ghosh <abhik.ghosh5@gmail.com>
2018-05-28 23:10:47 +0530

    eda added

---
+++ EDA/TwitterSourcing.R
@@ -0,0 +1,37 @@
+####### Twitter Data Collection ##################
+install.packages("twitteR")
+require("twitteR")   
+#Connect with Twitter API using below 
+options(httr_oauth_cache=T)
+consumer_key    = 'u2bX0cECiwe8Vjk9NhHG5x518'
+consumer_secret = 'F1lZY6yXtrafqq3V7tsXSi1VMrahwzmZhMhjuJPyHPV9dWu1sh'
+access_token    = '858753424271196162-G0RgoAiNLOM2nKYvntijxWwu6eSOHJE'
+access_secret   = 'Mxwwz8QS8cGmQXWKGTmCd2ZJNBb9F5BMTZc1xCeGQzxX3'
+download.file(url="http://curl.haxx.se/ca/cacert.pem",destfile="cacert.pem")
+setup_twitter_oauth(consumer_key,consumer_secret,access_token,access_secret)
+
+#Search Twitter with any string as 'search.string' 
+search.string = "tcs"
+data=searchTwitter(search.string,lang="en",resultType="recent",n=200)
+data <- do.call("rbind", lapply(data, as.data.frame))
+
+#################################################
+
+#Fetch Wikipedia
+library(devtools)
+install_github("Ironholds/WikipediR")
+library(WikipediR)
+library(tm.plugin.webmining)
+library(stringr)
+
+#Get Wikipedia content
+wp_content <- page_content("en","wikipedia",
+                           page_name = "India_national_cricket_team")
+
+
+html <- wp_content$parse$text
+
+#Parse HTML
+text=extractHTMLStrip(html)
+text=str_replace_all(text,"\n", " ")
+text=data.frame(text)
